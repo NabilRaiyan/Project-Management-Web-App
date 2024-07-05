412f348d8668bdbdace2b67ca9a9c736c820bc7c
NabilRaiyan <rraiyan77@gmail.com>
2024-07-05 22:30:45 +0600

    adding user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,7 +6,7 @@ use App\Http\Requests\StoreUserRequest;
 use App\Http\Requests\UpdateUserRequest;
 use App\Models\User;
 
-// 
+// creating user controller
 class UserController extends Controller
 {
     /**
