7f7653ad3d7781abd37e59e923d662bb8aa3376f
NabilRaiyan <rraiyan77@gmail.com>
2024-07-11 22:05:28 +0600

    adding project and task resources

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -15,6 +15,8 @@ class ProjectController extends Controller
      */
     public function index()
     {
+        $query = Project::query();
+        $projects = $query->paginate(10)->perPage(5);
         return inertia("Project/Index", []);
     }
 
