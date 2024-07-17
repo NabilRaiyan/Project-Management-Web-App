7844c3a9f0d28ba648e8df06a6f7933150802a4b
NabilRaiyan <rraiyan77@gmail.com>
2024-07-17 16:11:01 +0600

    creating new project form

--- resources/js/Pages/Project/Create.jsx
+++ resources/js/Pages/Project/Create.jsx
@@ -1,12 +1,32 @@
+import InputError from "@/Components/InputError";
+import InputLabel from "@/Components/InputLabel";
+import TextInput from "@/Components/TextInput";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-
+import { Head, useForm } from "@inertiajs/react";
+import TextAreaInput from "@/Components/TextAreaInput";
+import SelectInput from "@/Components/SelectInput";
 export default function Create({auth}){
+    // inirtia form variable
+    const {data, setData, post, reset, errors} = useForm({
+        image: '',
+        name: '',
+        status: '',
+        description: '',
+        due_date: ''
+    })
+
+    // onSubmit function to submit the form 
+    const onSubmit = (e)=>{
+        e.preventDefault();
+        post(route("project.create"))
+    }
+
     return (
         <AuthenticatedLayout
         user={auth.user}
         header= {
             <div className="flex justify-between items-center">
-                <h2 className="font-semibold text-xl text-gray-800 leading-tight">Projects</h2>
+                <h2 className="font-semibold text-xl text-gray-800 leading-tight">Create New Project</h2>
             </div>
             }
         >
@@ -15,7 +35,54 @@ export default function Create({auth}){
             <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div className="overflow-auto">
-                        kdjfnnflkjnvklf
+                        <form onSubmit={onSubmit} className="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                            {/* project image */}
+                            <div>
+                                <InputLabel className="text-white mb-4 text-xl" htmlFor="project_image_path" value="Project Image"></InputLabel>
+                                <TextInput id="project_image_path" type="file" name="image" 
+                                value={data.image} className="mt-1 block w-full text-white" onChange={e => setData('image', e.target.value)}></TextInput>
+                                <InputError message={errors.image} className="mt-3"></InputError>
+                            </div>
+                            {/* project name */}
+                            <div className="mt-6">
+                                <InputLabel className="text-white mb-4 text-xl" htmlFor="project_name" value="Project Name"></InputLabel>
+                                <TextInput id="project_name" type="text" name="name" isFocused={true}
+                                value={data.name} className="mt-1 block w-full text-white caret-gray-800" onChange={e => setData('name', e.target.value)}></TextInput>
+                                <InputError message={errors.name} className="mt-3"></InputError>
+                            </div>
+
+                            {/* project description */}
+                            <div className="mt-6">
+                                <InputLabel className="text-white mb-4 text-xl" htmlFor="project_description" value="Project Description"></InputLabel>
+                                <TextAreaInput id="project_description" type="text" name="description"
+                                value={data.description} className="mt-1 block w-full text-white caret-gray-800" onChange={e => setData('description', e.target.value)}></TextAreaInput>
+                                <InputError message={errors.description} className="mt-3"></InputError>
+                            </div>
+
+                            {/* project status */}
+                            <div className="mt-6">
+                                <InputLabel className="text-white mb-4 text-xl" htmlFor="project_status" value="Project Status"></InputLabel>
+                                <SelectInput id="project_status" type="text" name="status"
+                                value={data.status} className="mt-1 block w-full text-gray-700" onChange={e => setData('status', e.target.value)}>
+                                    <option value="">Select Status</option>
+                                    <option value="pending">Pending</option>
+                                    <option value="in_progress">In Progress</option>
+                                    <option value="completed">Completed</option>
+
+                                </SelectInput>
+                                <InputError message={errors.status} className="mt-3"></InputError>
+                            </div>
+
+
+                            {/* project due_date */}
+                            <div className="mt-6">
+                                <InputLabel className="text-white mb-4 text-xl" htmlFor="project_due_date" value="Project Due Date"></InputLabel>
+                                <TextInput id="project_due_date" type="date" name="due_date"
+                                value={data.due_date} className="mt-1 block w-full text-gray-800" onChange={e => setData('due_date', e.target.value)}></TextInput>
+                                <InputError message={errors.due_date} className="mt-3"></InputError>
+                            </div>
+                            
+                        </form>
                     </div>
                 </div>
             </div>
