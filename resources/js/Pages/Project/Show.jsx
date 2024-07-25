10b91379d8e940ed0ef3c10c47fc8f706777ee53
NabilRaiyan <rraiyan77@gmail.com>
2024-07-25 17:51:01 +0600

    adding index page

--- resources/js/Pages/Project/Show.jsx
+++ resources/js/Pages/Project/Show.jsx
@@ -78,7 +78,7 @@ export default function Show({auth, project, tasks, queryParams}){
             </div>
         </div>
 
-        {/* Task table  */}
+        {/* Task table show  */}
         <div className="pb-12">
             <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg py-5 px-6">
